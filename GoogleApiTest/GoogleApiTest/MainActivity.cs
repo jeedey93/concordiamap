b16e3ed03f373e07753cec60d88d26b597e3f7c5
darthnihil <rbrunet09@yahoo.ca>
2015-01-29 17:02:50 -0500

    SplashScreen Started

--- GoogleApiTest/GoogleApiTest/MainActivity.cs
+++ GoogleApiTest/GoogleApiTest/MainActivity.cs
@@ -9,17 +9,16 @@ using Android.OS;
 
 namespace GoogleApiTest
 {
-	[Activity (Label = "GoogleApiTest", MainLauncher = true, Icon = "@drawable/icon")]
+	[Activity (Label = "GoogleApiTest", MainLauncher = true, Icon = "@drawable/icon", Theme = "@style/Theme.Splash")]
 	public class MainActivity : Activity
 	{
 		protected override void OnCreate (Bundle bundle)
 		{
 			base.OnCreate (bundle);
-
+			System.Threading.Thread.Sleep (3000);
+			var activity1 = new Intent (this, typeof(MapActivity));
+			StartActivity (activity1);
 			var button = FindViewById<ImageButton> (Resource.Id.locateMe);
-			// Set our view from the "main" layout resource
-			SetContentView (Resource.Layout.Main);
-
 		}
 	}
 }
