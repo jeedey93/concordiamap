93d2032bdfbdcafe898251bc3fe536a407cfe8ab
ephilippona <ephilippona@gmail.com>
2015-03-08 21:40:20 -0400

    Revert "Revert "Merge remote-tracking branch 'origin/master'""
    This reverts commit 0d3711a152732c094957a9620d614424115c8a98.

--- GoogleApiTest/GoogleApiTest/Models/DirectionFetcher.cs
+++ GoogleApiTest/GoogleApiTest/Models/DirectionFetcher.cs
@@ -59,6 +59,49 @@ namespace GoogleApiTest
 			return allInstructions;
 		}
 
+
+		public string GetInstructionsDifferentCampus(JsonValue results, JsonValue results2){
+			string allInstructions = "";
+
+			//START ADDRESS
+			string startAdress = results [0] ["legs"][0]["start_address"];
+			string endAdress = results2 [0] ["legs"][0]["end_address"];
+
+			int instructionNumber = 0;
+			//INSTRUCTIONS
+			string instructions="\r\n";
+			int numberSteps = results [0] ["legs"] [0] ["steps"].Count;
+			for (int i = 0; i < numberSteps; i++) {
+				instructionNumber++;
+				instructions += instructionNumber + "." +results [0] ["legs"] [0] ["steps"] [i] ["html_instructions"] + "\r\n";
+			}
+
+			instructionNumber++;
+			instructions += instructionNumber + "." +"Take the bus to the other campus\r\n";
+
+			//INSTRUCTIONS
+			int numberSteps2 = results2 [0] ["legs"] [0] ["steps"].Count;
+			for (int i = 0; i < numberSteps2; i++) {
+				instructionNumber++;
+				instructions += instructionNumber + "." +results2 [0] ["legs"] [0] ["steps"] [i] ["html_instructions"] + "\r\n";
+			}
+
+			string duration = results [0] ["legs"][0]["duration"]["text"] +" + bus travel time (~20mins) + "+ results2 [0] ["legs"][0]["duration"]["text"];
+
+			//DISTANCE IN KM = firstRoutesResults [0] ["overview_polyline"] ["points"];
+			//DURATION IN MIN = firstRoutesResults [0] ["legs"][0]["duration"]["text"];
+			//START ADDRESS = firstRoutesResults [0] ["legs"][0]["start_address"];
+			//END ADDRESS = firstRoutesResults [0] ["legs"][0]["end_address"];
+
+
+			//INSTRUCTIONS = 
+			//	firstRoutesResults [0] ["legs"][0]["steps"].Count
+			//	firstRoutesResults [0] ["legs"][0]["steps"][0]["html_instructions"];
+
+			allInstructions = "START:\r\n" + startAdress + "\r\nEND:\r\n"+  endAdress + "\r\nDURATION:\r\n" + duration + "\r\nINSTRUCTIONS :" + instructions;
+			return allInstructions;
+		}
+
 		public List<LatLng> DecodePolylinePoints(string encodedPoints) 
 		{
 			if (encodedPoints == null || encodedPoints == "") return null;
