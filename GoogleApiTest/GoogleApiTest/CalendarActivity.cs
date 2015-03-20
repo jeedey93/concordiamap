9bac41fc9b6cec9ae44fd07710b2ae8809b84f76
jeedey93 <justin.do@hotmail.com>
2015-03-19 21:32:04 -0400

    Google + Sign In Working

--- GoogleApiTest/GoogleApiTest/CalendarActivity.cs
+++ GoogleApiTest/GoogleApiTest/CalendarActivity.cs
@@ -10,22 +10,135 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using Android.Gms.Common.Apis;
+using Android.Gms.Common;
+using Android.Gms.Plus;
+using Android.Gms.Plus.Model.People;
 
 namespace GoogleApiTest
 {
 	[Activity (Label = "CalendarActivity")]			
-	public class CalendarActivity : LeftDrawerActivity
+	public class CalendarActivity : Activity, IGoogleApiClientConnectionCallbacks, IGoogleApiClientOnConnectionFailedListener
 	{
+		private IGoogleApiClient mGoogleApiClient;
+		private SignInButton mGoogleSignIn;
+
+		private bool mIntentInProgress;
+		private bool mSignInClicked;
+		private bool mInfoPopulated;
+
+		private ConnectionResult mConnectionResult;
+
 		protected override void OnCreate (Bundle bundle)
 		{
 			base.OnCreate (bundle);
+			SetContentView (Resource.Layout.Calendar);
 
-			// Create your application here
 
-			GoogleCalendar calendar = new GoogleCalendar
-				("Justin Do", "Google account name", "Google account password");
-			CalendarEventObject[] events = calendar.GetEvents ();
-			Console.WriteLine (events);
+			mGoogleSignIn = FindViewById<SignInButton> (Resource.Id.sign_in_button);
+			mGoogleSignIn.Click += mGoogleSignIn_Click;
+
+			GoogleApiClientBuilder builder = new GoogleApiClientBuilder (this);
+			builder.AddConnectionCallbacks (this);
+			builder.AddOnConnectionFailedListener (this);
+			builder.AddApi(PlusClass.Api);
+			builder.AddScope (PlusClass.ScopePlusProfile);
+			builder.AddScope (PlusClass.ScopePlusLogin);
+
+			//Build our IGoogleApiClient
+			mGoogleApiClient = builder.Build ();
+
+
+
+			//GoogleCalendar calendar = new GoogleCalendar
+			//	("Justin Do", "Google account name", "Google account password");
+			//CalendarEventObject[] events = calendar.GetEvents ();
+			//Console.WriteLine (events);
+		}
+
+		void mGoogleSignIn_Click(object sender, EventArgs e){
+			//Fire Sign In
+			if (!mGoogleApiClient.IsConnecting) {
+				mSignInClicked = true;
+				ResolveSignInError ();
+			}
+		}
+
+		private void ResolveSignInError(){
+			if (mGoogleApiClient.IsConnected) {
+				//No need to resolve errors, already connected
+				return;
+			}
+
+			if (mConnectionResult.HasResolution) {
+				try{
+				mIntentInProgress = true;
+				StartIntentSenderForResult (mConnectionResult.Resolution.IntentSender, 0, null, 0, 0, 0);
+				}
+				catch(Android.Content.IntentSender.SendIntentException e){
+					// The intent was cancelled before it was sent. Return to the default 
+					// state and attempt to connect to get an updated ConnectionResult
+					mIntentInProgress = false;
+					mGoogleApiClient.Connect (); 
+				}
+			}
+		}
+
+		protected override void OnActivityResult (int requestCode, Result resultCode, Intent data)
+		{
+			if (requestCode == 0) {
+				if (resultCode != Result.Ok) {
+					mSignInClicked = false;
+				}
+
+				mIntentInProgress = false;
+
+				if (!mGoogleApiClient.IsConnecting) {
+					mGoogleApiClient.Connect ();
+				}
+			}
+		}
+
+		protected override void OnStart ()
+		{
+			base.OnStart ();
+			mGoogleApiClient.Connect ();
+		}
+
+		protected override void OnStop(){
+			base.OnStop ();
+			if (mGoogleApiClient.IsConnected) {
+				mGoogleApiClient.Disconnect ();
+			}
+		}
+
+		public void OnConnected (Bundle connectionHint)
+		{
+			//Successful log in
+			mSignInClicked = false;
+
+			if (PlusClass.PeopleApi.GetCurrentPerson (mGoogleApiClient) != null) {
+				IPerson plusUser = PlusClass.PeopleApi.GetCurrentPerson (mGoogleApiClient);
+				Console.WriteLine (plusUser.DisplayName);
+			}
+		}
+
+		public void OnConnectionSuspended (int cause)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public void OnConnectionFailed (ConnectionResult result)
+		{
+			if (!mIntentInProgress) {
+				//Store the ConnectionResults so that we can use it later when the user click sign in
+				mConnectionResult = result;
+			}
+
+			if (mSignInClicked) {
+				//The user has already clicked sign in so we attempt to resolve all errors until the user is signed in or the cancel
+				ResolveSignInError ();
+			}
 		}
 	}
 }
--- GoogleApiTest/GoogleApiTest/Resources/Resource.designer.cs
+++ GoogleApiTest/GoogleApiTest/Resources/Resource.designer.cs
@@ -1906,35 +1906,35 @@ namespace GoogleApiTest
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f0b0067
-			public const int AutoCompleteInput = 2131427431;
-			
-			// aapt resource value: 0x7f0b006c
-			public const int Driving = 2131427436;
-			
-			// aapt resource value: 0x7f0b007a
-			public const int From = 2131427450;
-			
-			// aapt resource value: 0x7f0b0070
-			public const int Reload = 2131427440;
-			
-			// aapt resource value: 0x7f0b007c
-			public const int Search = 2131427452;
-			
-			// aapt resource value: 0x7f0b0072
-			public const int SlideUpList = 2131427442;
-			
-			// aapt resource value: 0x7f0b0071
-			public const int SlideUpText = 2131427441;
-			
-			// aapt resource value: 0x7f0b007b
-			public const int To = 2131427451;
+			// aapt resource value: 0x7f0b0068
+			public const int AutoCompleteInput = 2131427432;
 			
 			// aapt resource value: 0x7f0b006d
-			public const int Transit = 2131427437;
+			public const int Driving = 2131427437;
 			
-			// aapt resource value: 0x7f0b006b
-			public const int Walking = 2131427435;
+			// aapt resource value: 0x7f0b007b
+			public const int From = 2131427451;
+			
+			// aapt resource value: 0x7f0b0071
+			public const int Reload = 2131427441;
+			
+			// aapt resource value: 0x7f0b007d
+			public const int Search = 2131427453;
+			
+			// aapt resource value: 0x7f0b0073
+			public const int SlideUpList = 2131427443;
+			
+			// aapt resource value: 0x7f0b0072
+			public const int SlideUpText = 2131427442;
+			
+			// aapt resource value: 0x7f0b007c
+			public const int To = 2131427452;
+			
+			// aapt resource value: 0x7f0b006e
+			public const int Transit = 2131427438;
+			
+			// aapt resource value: 0x7f0b006c
+			public const int Walking = 2131427436;
 			
 			// aapt resource value: 0x7f0b0045
 			public const int action_bar = 2131427397;
@@ -2023,17 +2023,17 @@ namespace GoogleApiTest
 			// aapt resource value: 0x7f0b002f
 			public const int classic = 2131427375;
 			
-			// aapt resource value: 0x7f0b006e
-			public const int clearLayout = 2131427438;
-			
 			// aapt resource value: 0x7f0b006f
-			public const int clearMarker = 2131427439;
+			public const int clearLayout = 2131427439;
+			
+			// aapt resource value: 0x7f0b0070
+			public const int clearMarker = 2131427440;
 			
 			// aapt resource value: 0x7f0b0017
 			public const int collapseActionView = 2131427351;
 			
-			// aapt resource value: 0x7f0b0059
-			public const int content_frame = 2131427417;
+			// aapt resource value: 0x7f0b005a
+			public const int content_frame = 2131427418;
 			
 			// aapt resource value: 0x7f0b0043
 			public const int decor_content_parent = 2131427395;
@@ -2050,8 +2050,8 @@ namespace GoogleApiTest
 			// aapt resource value: 0x7f0b002e
 			public const int donate_with_google = 2131427374;
 			
-			// aapt resource value: 0x7f0b0058
-			public const int drawer_layout = 2131427416;
+			// aapt resource value: 0x7f0b0059
+			public const int drawer_layout = 2131427417;
 			
 			// aapt resource value: 0x7f0b001c
 			public const int dropdown = 2131427356;
@@ -2068,29 +2068,29 @@ namespace GoogleApiTest
 			// aapt resource value: 0x7f0b003c
 			public const int expanded_menu = 2131427388;
 			
-			// aapt resource value: 0x7f0b005c
-			public const int exploreLTCount = 2131427420;
-			
-			// aapt resource value: 0x7f0b005e
-			public const int exploreLTDistance = 2131427422;
-			
 			// aapt resource value: 0x7f0b005d
-			public const int exploreLTTitle = 2131427421;
-			
-			// aapt resource value: 0x7f0b005a
-			public const int exploreLView = 2131427418;
-			
-			// aapt resource value: 0x7f0b0061
-			public const int exploreMBBar = 2131427425;
-			
-			// aapt resource value: 0x7f0b0060
-			public const int exploreMBCoffee = 2131427424;
-			
-			// aapt resource value: 0x7f0b0062
-			public const int exploreMBPharmacy = 2131427426;
+			public const int exploreLTCount = 2131427421;
 			
 			// aapt resource value: 0x7f0b005f
-			public const int exploreMBRestaurant = 2131427423;
+			public const int exploreLTDistance = 2131427423;
+			
+			// aapt resource value: 0x7f0b005e
+			public const int exploreLTTitle = 2131427422;
+			
+			// aapt resource value: 0x7f0b005b
+			public const int exploreLView = 2131427419;
+			
+			// aapt resource value: 0x7f0b0062
+			public const int exploreMBBar = 2131427426;
+			
+			// aapt resource value: 0x7f0b0061
+			public const int exploreMBCoffee = 2131427425;
+			
+			// aapt resource value: 0x7f0b0063
+			public const int exploreMBPharmacy = 2131427427;
+			
+			// aapt resource value: 0x7f0b0060
+			public const int exploreMBRestaurant = 2131427424;
 			
 			// aapt resource value: 0x7f0b0030
 			public const int grayscale = 2131427376;
@@ -2119,17 +2119,17 @@ namespace GoogleApiTest
 			// aapt resource value: 0x7f0b0037
 			public const int image = 2131427383;
 			
-			// aapt resource value: 0x7f0b0069
-			public const int imageButton2 = 2131427433;
+			// aapt resource value: 0x7f0b006a
+			public const int imageButton2 = 2131427434;
 			
-			// aapt resource value: 0x7f0b0079
-			public const int layout = 2131427449;
+			// aapt resource value: 0x7f0b007a
+			public const int layout = 2131427450;
 			
-			// aapt resource value: 0x7f0b005b
-			public const int left_drawer = 2131427419;
+			// aapt resource value: 0x7f0b005c
+			public const int left_drawer = 2131427420;
 			
-			// aapt resource value: 0x7f0b007e
-			public const int linearLayout1 = 2131427454;
+			// aapt resource value: 0x7f0b007f
+			public const int linearLayout1 = 2131427455;
 			
 			// aapt resource value: 0x7f0b0009
 			public const int listMode = 2131427337;
@@ -2137,38 +2137,38 @@ namespace GoogleApiTest
 			// aapt resource value: 0x7f0b0039
 			public const int list_item = 2131427385;
 			
-			// aapt resource value: 0x7f0b0068
-			public const int load_to_direction = 2131427432;
+			// aapt resource value: 0x7f0b0069
+			public const int load_to_direction = 2131427433;
 			
-			// aapt resource value: 0x7f0b0078
-			public const int logo = 2131427448;
+			// aapt resource value: 0x7f0b0079
+			public const int logo = 2131427449;
 			
-			// aapt resource value: 0x7f0b0064
-			public const int map = 2131427428;
+			// aapt resource value: 0x7f0b0065
+			public const int map = 2131427429;
 			
 			// aapt resource value: 0x7f0b002a
 			public const int match_parent = 2131427370;
 			
-			// aapt resource value: 0x7f0b0076
-			public const int media_route_control_frame = 2131427446;
-			
 			// aapt resource value: 0x7f0b0077
-			public const int media_route_disconnect_button = 2131427447;
+			public const int media_route_control_frame = 2131427447;
 			
-			// aapt resource value: 0x7f0b0073
-			public const int media_route_list = 2131427443;
+			// aapt resource value: 0x7f0b0078
+			public const int media_route_disconnect_button = 2131427448;
 			
 			// aapt resource value: 0x7f0b0074
-			public const int media_route_volume_layout = 2131427444;
+			public const int media_route_list = 2131427444;
 			
 			// aapt resource value: 0x7f0b0075
-			public const int media_route_volume_slider = 2131427445;
+			public const int media_route_volume_layout = 2131427445;
+			
+			// aapt resource value: 0x7f0b0076
+			public const int media_route_volume_slider = 2131427446;
 			
 			// aapt resource value: 0x7f0b0015
 			public const int middle = 2131427349;
 			
-			// aapt resource value: 0x7f0b006a
-			public const int mode = 2131427434;
+			// aapt resource value: 0x7f0b006b
+			public const int mode = 2131427435;
 			
 			// aapt resource value: 0x7f0b0031
 			public const int monochrome = 2131427377;
@@ -2245,8 +2245,11 @@ namespace GoogleApiTest
 			// aapt resource value: 0x7f0b0011
 			public const int showTitle = 2131427345;
 			
-			// aapt resource value: 0x7f0b0063
-			public const int sliding_layout = 2131427427;
+			// aapt resource value: 0x7f0b0058
+			public const int sign_in_button = 2131427416;
+			
+			// aapt resource value: 0x7f0b0064
+			public const int sliding_layout = 2131427428;
 			
 			// aapt resource value: 0x7f0b0007
 			public const int split_action_bar = 2131427335;
@@ -2266,11 +2269,11 @@ namespace GoogleApiTest
 			// aapt resource value: 0x7f0b003b
 			public const int title = 2131427387;
 			
-			// aapt resource value: 0x7f0b0066
-			public const int togglebutton = 2131427430;
+			// aapt resource value: 0x7f0b0067
+			public const int togglebutton = 2131427431;
 			
-			// aapt resource value: 0x7f0b0065
-			public const int toolbar = 2131427429;
+			// aapt resource value: 0x7f0b0066
+			public const int toolbar = 2131427430;
 			
 			// aapt resource value: 0x7f0b0008
 			public const int up = 2131427336;
@@ -2278,11 +2281,11 @@ namespace GoogleApiTest
 			// aapt resource value: 0x7f0b0012
 			public const int useLogo = 2131427346;
 			
-			// aapt resource value: 0x7f0b007d
-			public const int view1 = 2131427453;
+			// aapt resource value: 0x7f0b007e
+			public const int view1 = 2131427454;
 			
-			// aapt resource value: 0x7f0b007f
-			public const int view2 = 2131427455;
+			// aapt resource value: 0x7f0b0080
+			public const int view2 = 2131427456;
 			
 			// aapt resource value: 0x7f0b001a
 			public const int withText = 2131427354;
@@ -2395,37 +2398,40 @@ namespace GoogleApiTest
 			public const int BuildingDescription = 2130903063;
 			
 			// aapt resource value: 0x7f030018
-			public const int ExploreListView = 2130903064;
+			public const int Calendar = 2130903064;
 			
 			// aapt resource value: 0x7f030019
-			public const int ExploreListView_Row = 2130903065;
+			public const int ExploreListView = 2130903065;
 			
 			// aapt resource value: 0x7f03001a
-			public const int ExploreView = 2130903066;
+			public const int ExploreListView_Row = 2130903066;
 			
 			// aapt resource value: 0x7f03001b
-			public const int LeftDrawer = 2130903067;
+			public const int ExploreView = 2130903067;
 			
 			// aapt resource value: 0x7f03001c
-			public const int list_locations = 2130903068;
+			public const int LeftDrawer = 2130903068;
 			
 			// aapt resource value: 0x7f03001d
-			public const int Main = 2130903069;
+			public const int list_locations = 2130903069;
 			
 			// aapt resource value: 0x7f03001e
-			public const int mr_media_route_chooser_dialog = 2130903070;
+			public const int Main = 2130903070;
 			
 			// aapt resource value: 0x7f03001f
-			public const int mr_media_route_controller_dialog = 2130903071;
+			public const int mr_media_route_chooser_dialog = 2130903071;
 			
 			// aapt resource value: 0x7f030020
-			public const int mr_media_route_list_item = 2130903072;
+			public const int mr_media_route_controller_dialog = 2130903072;
 			
 			// aapt resource value: 0x7f030021
-			public const int Navigate = 2130903073;
+			public const int mr_media_route_list_item = 2130903073;
 			
 			// aapt resource value: 0x7f030022
-			public const int support_simple_spinner_dropdown_item = 2130903074;
+			public const int Navigate = 2130903074;
+			
+			// aapt resource value: 0x7f030023
+			public const int support_simple_spinner_dropdown_item = 2130903075;
 			
 			static Layout()
 			{
