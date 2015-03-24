a674822cb5b972f65ee65db5b3111a1c5adbe537
ephilippona <ephilippona@gmail.com>
2015-03-23 20:54:23 -0400

    More Bus Schedule  & opacity to drawers

--- GoogleApiTest/GoogleApiTest/BusScheduleActivity.cs
+++ GoogleApiTest/GoogleApiTest/BusScheduleActivity.cs
@@ -6,15 +6,96 @@ using Android.OS;
 using Android.Support.V4.Widget;
 using Android.Support.V4.App;
 using Android.Views;
+using System.Collections.Generic;
 
 namespace GoogleApiTest
 {
 	[Activity (Label = "BusScheduleActivity")]			
 	public class BusScheduleActivity : LeftDrawerActivity
 	{
+		BusManager BusManager = new BusManager();
+		List<string> LOYMonThur = new List<string>();
+		List<string> LOYFriday = new List<string>();
+		List<string> SGWMonThur = new List<string>();
+		List<string> SGWFriday = new List<string>();
+		Button MonThurButton;
+		Button FridayButton;
+
+
 		protected override void OnCreate (Bundle bundle)
 		{
 			base.OnCreate (bundle,Resource.Layout.BusSchedule);
+
+			LOYFriday = BusManager.InitializeLOYBusFriday ();
+			LOYMonThur = BusManager.InitializeLOYBusMonThur ();
+			SGWFriday = BusManager.InitializeSGWBusFriday ();
+			SGWMonThur = BusManager.InitializeSGWBusMonThur ();
+		
+			MonThurButton = (Button)FindViewById (Resource.Id.buttonMonThur);
+			FridayButton = (Button)FindViewById (Resource.Id.buttonFriday);
+
+
+			MonThurButton.SetBackgroundColor (new Android.Graphics.Color(146,35,56,255));
+			FridayButton.SetBackgroundColor (new Android.Graphics.Color (203,181,118,150));
+
+			ArrayAdapter<string> itemsAdapter = new ArrayAdapter<string>(this,Android.Resource.Layout.SimpleListItem1, SGWMonThur);
+			ArrayAdapter<string> itemsAdapter1 = new ArrayAdapter<string>(this,Android.Resource.Layout.SimpleListItem1, LOYMonThur);
+
+
+			ListView listViewSGW = (ListView) FindViewById(Resource.Id.SGWBusListView);
+			listViewSGW.SetAdapter (itemsAdapter);
+
+			ListView listViewLOY = (ListView) FindViewById(Resource.Id.LOYBusListView);
+			listViewLOY.SetAdapter (itemsAdapter1);
+
+			listViewSGW.SetBackgroundColor(new Android.Graphics.Color(235,221,183,0));
+
+			MonThurButton.Click += (sender, e) => {
+				//Do Something
+				MonThurButtonClick(itemsAdapter, itemsAdapter1);
+
+				MonThurButton.SetBackgroundColor (new Android.Graphics.Color(146,35,56,255));
+				FridayButton.SetBackgroundColor (new Android.Graphics.Color (203,181,118,150));
+
+			};
+
+			FridayButton.Click += (sender, e) => {
+				//Do Something
+				FridayButtonClick(itemsAdapter, itemsAdapter1);
+
+				MonThurButton.SetBackgroundColor (new Android.Graphics.Color(203,181,118,150));
+				FridayButton.SetBackgroundColor (new Android.Graphics.Color (146,35,56,255));
+
+			}; 
+
+
+
+			}
+		void MonThurButtonClick(ArrayAdapter<string> itemsAdapter, ArrayAdapter<string> itemsAdapter1 ){
+
+			itemsAdapter = new ArrayAdapter<string> (this, Android.Resource.Layout.SimpleListItem1, SGWMonThur);
+			itemsAdapter1 = new ArrayAdapter<string>(this,Android.Resource.Layout.SimpleListItem1, LOYMonThur);
+			
+			ListView listViewSGW = (ListView) FindViewById(Resource.Id.SGWBusListView);
+			listViewSGW.SetAdapter (itemsAdapter);
+
+			ListView listViewLOY = (ListView) FindViewById(Resource.Id.LOYBusListView);
+			listViewLOY.SetAdapter (itemsAdapter1);
+
 		}
+
+		void FridayButtonClick( ArrayAdapter<string> itemsAdapter, ArrayAdapter<string> itemsAdapter1 ){
+
+			itemsAdapter = new ArrayAdapter<string> (this, Android.Resource.Layout.SimpleListItem1, SGWFriday);
+			itemsAdapter1 = new ArrayAdapter<string>(this,Android.Resource.Layout.SimpleListItem1, LOYFriday);
+
+			ListView listViewSGW = (ListView) FindViewById(Resource.Id.SGWBusListView);
+			listViewSGW.SetAdapter (itemsAdapter);
+
+			ListView listViewLOY = (ListView) FindViewById(Resource.Id.LOYBusListView);
+			listViewLOY.SetAdapter (itemsAdapter1);
+
+		}
+
 	}
 }
---
+++ GoogleApiTest/GoogleApiTest/Models/BusManager.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GoogleApiTest
+{
+	public class BusManager
+	{
+		List<String> LOYBusMonThur = new List<String>();
+		List<String> SGWBusMonThur = new List<String>();
+		List<String> SGWBusFriday = new List<String>();
+		List<String> LOYBusFriday = new List<String>();
+
+		 
+
+
+		public List<String> InitializeLOYBusMonThur(){
+				
+			LOYBusMonThur.Add ("7:45");
+			LOYBusMonThur.Add ("8:00");
+			LOYBusMonThur.Add ("8:15");
+			LOYBusMonThur.Add ("8:30");
+			LOYBusMonThur.Add ("8:45");
+			LOYBusMonThur.Add ("9:00");
+			LOYBusMonThur.Add ("9:15");
+			LOYBusMonThur.Add ("9:30");
+			LOYBusMonThur.Add ("9:45");
+			LOYBusMonThur.Add ("10:00");
+			LOYBusMonThur.Add ("10:15");
+			LOYBusMonThur.Add ("10:30");
+			LOYBusMonThur.Add ("10:55");
+			LOYBusMonThur.Add ("11:15");
+			LOYBusMonThur.Add ("From 11:30 to 19:00\n\nA bus leaves each campus every 20 – 25 minutes.");
+			LOYBusMonThur.Add ("19:00");
+			LOYBusMonThur.Add ("19:30");
+			LOYBusMonThur.Add ("20:00");
+			LOYBusMonThur.Add ("20:15");
+			LOYBusMonThur.Add ("20:30");
+			LOYBusMonThur.Add ("20:45");
+			LOYBusMonThur.Add ("21:10");
+			LOYBusMonThur.Add ("21:35");
+			LOYBusMonThur.Add ("22:00");
+			LOYBusMonThur.Add ("22:30");
+			LOYBusMonThur.Add ("23:00");
+		
+			return LOYBusMonThur;
+		}
+
+		public List<String> InitializeSGWBusMonThur(){
+
+			SGWBusMonThur.Add ("7:45");
+			SGWBusMonThur.Add ("8:30");
+			SGWBusMonThur.Add ("8:45");
+			SGWBusMonThur.Add ("9:00");
+			SGWBusMonThur.Add ("9:15");
+			SGWBusMonThur.Add ("9:30");
+			SGWBusMonThur.Add ("9:45");
+			SGWBusMonThur.Add ("10:00");
+			SGWBusMonThur.Add ("10:15");
+			SGWBusMonThur.Add ("10:30");
+			SGWBusMonThur.Add ("11:00");
+			SGWBusMonThur.Add ("11:45");
+			SGWBusMonThur.Add ("From 11:30 to 19:00\n\nA bus leaves each campus every 20 – 25 minutes.");
+			SGWBusMonThur.Add ("19:00");
+			SGWBusMonThur.Add ("19:30");
+			SGWBusMonThur.Add ("19:45");
+			SGWBusMonThur.Add ("20:00");
+			SGWBusMonThur.Add ("20:15");
+			SGWBusMonThur.Add ("20:45");
+			SGWBusMonThur.Add ("21:00");
+			SGWBusMonThur.Add ("21:10");
+			SGWBusMonThur.Add ("21:35");
+			SGWBusMonThur.Add ("22:00");
+			SGWBusMonThur.Add ("22:30");
+			SGWBusMonThur.Add ("23:00");
+
+			return SGWBusMonThur;
+		}
+
+
+		public List<String> InitializeLOYBusFriday(){
+
+			LOYBusFriday.Add ("7:45");
+			LOYBusFriday.Add ("8:15");
+			LOYBusFriday.Add ("8:45");
+			LOYBusFriday.Add ("9:00");
+			LOYBusFriday.Add ("9:15");
+			LOYBusFriday.Add ("9:45");
+			LOYBusFriday.Add ("10:00");
+			LOYBusFriday.Add ("10:15");
+			LOYBusFriday.Add ("11:00");
+			LOYBusFriday.Add ("11:15");
+			LOYBusFriday.Add ("11:45");
+			LOYBusFriday.Add ("12:00");
+			LOYBusFriday.Add ("12:15");
+			LOYBusFriday.Add ("12:45");
+			LOYBusFriday.Add ("13:15");
+			LOYBusFriday.Add ("13:30");
+			LOYBusFriday.Add ("13:45");
+			LOYBusFriday.Add ("14:15");
+			LOYBusFriday.Add ("14:30");
+			LOYBusFriday.Add ("14:45");
+			LOYBusFriday.Add ("15:15");
+			LOYBusFriday.Add ("15:30");
+			LOYBusFriday.Add ("15:45");
+			LOYBusFriday.Add ("16:15");
+			LOYBusFriday.Add ("16:30");
+			LOYBusFriday.Add ("16:45");
+			LOYBusFriday.Add ("17:15");
+			LOYBusFriday.Add ("18:15");
+			LOYBusFriday.Add ("18:45");
+			LOYBusFriday.Add ("19:15");
+			LOYBusFriday.Add ("19:45");
+
+			return SGWBusMonThur;
+		}
+
+
+		public List<String> InitializeSGWBusFriday(){
+
+			SGWBusFriday.Add ("7:45");
+			SGWBusFriday.Add ("8:15");
+			SGWBusFriday.Add ("8:45");
+			SGWBusFriday.Add ("9:15");
+			SGWBusFriday.Add ("9:30");
+			SGWBusFriday.Add ("9:45");
+			SGWBusFriday.Add ("10:15");
+			SGWBusFriday.Add ("10:30");
+			SGWBusFriday.Add ("10:45");
+			SGWBusFriday.Add ("11:30");
+			SGWBusFriday.Add ("11:45");
+			SGWBusFriday.Add ("12:15");
+			SGWBusFriday.Add ("12:45");
+			SGWBusFriday.Add ("13:00");
+			SGWBusFriday.Add ("13.15");
+			SGWBusFriday.Add ("13:45");
+			SGWBusFriday.Add ("14:00");
+			SGWBusFriday.Add ("14:15");
+			SGWBusFriday.Add ("14:45");
+			SGWBusFriday.Add ("15:00");
+			SGWBusFriday.Add ("15:15");
+			SGWBusFriday.Add ("15:45");
+			SGWBusFriday.Add ("16:00");
+			SGWBusFriday.Add ("16:15");
+			SGWBusFriday.Add ("16:45");
+			SGWBusFriday.Add ("17:15");
+			SGWBusFriday.Add ("17:45");
+			SGWBusFriday.Add ("18:15");
+			SGWBusFriday.Add ("18:45");
+			SGWBusFriday.Add ("19:15");
+			SGWBusFriday.Add ("19:45");
+
+			return SGWBusMonThur;
+		}
+	}
+}
