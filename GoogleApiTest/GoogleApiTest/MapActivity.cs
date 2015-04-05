c00d5b5fb014ced666add4cc7666c9f2804539d4
undueKonata <liem.q.ma@gmail.com>
2015-04-05 17:56:26 -0400

    Moved price from walking to transit mode

--- GoogleApiTest/GoogleApiTest/MapActivity.cs
+++ GoogleApiTest/GoogleApiTest/MapActivity.cs
@@ -699,12 +699,12 @@ namespace GoogleApiTest
 
 			JsonValue routesResults = directions ["routes"];
 
-			GetInstructionSameCampus (routesResults);
+			GetInstructionSameCampus (routesResults, transitMode);
 		}
 
-		void GetInstructionSameCampus (JsonValue routesResults)
+		void GetInstructionSameCampus (JsonValue routesResults,String transitMode)
 		{
-			string instructions = DirectionFetcher.GetInstructions (routesResults);
+			string instructions = DirectionFetcher.GetInstructions (routesResults, transitMode);
 		
 			string formattedInstructions = DisplayStepDirections (instructions);
 
