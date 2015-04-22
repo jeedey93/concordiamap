1703a1499b52498b742dc7d6bb84997474360800
jeedey93 <justin.do@hotmail.com>
2015-04-22 00:30:27 -0400

    Delete GData

--- GoogleApiTest/GoogleApiTest/Properties/AssemblyInfo.cs
+++ GoogleApiTest/GoogleApiTest/Properties/AssemblyInfo.cs
@@ -26,3 +26,10 @@ using Android.App;
 //[assembly: AssemblyDelaySign(false)]
 //[assembly: AssemblyKeyFile("")]
 
+
+#if DEBUG
+[assembly: Application(Debuggable=true)]
+#else
+[assembly: Application(Debuggable=false)]
+#endif
+
